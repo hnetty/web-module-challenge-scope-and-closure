cbf462c784f610a95406272f9d3ffc9127a07b74
Harper Nettesheim <harpernettesheim@Harpers-MacBook-Pro.local>
2020-05-18 16:43:25 -0500

    Final changes of the day

--- index.js
+++ index.js
@@ -28,10 +28,18 @@ function processFirstItem(stringList, callback) {
  * 
  * 1. What is the difference between counter1 and counter2?
  * 
+ *      -The variable in counter1 example is a local variable and can only be used inside 
+ *  the loop, which is why it must be returned. On the other hand, counter2 example's variable
+ *  a global variable and can be accesed anywhere in the code
+ * 
  * 2. Which of the two uses a closure? How can you tell?
  * 
+ *      -Counter1 uses a closure because it is calling a function inside a function
+ * 
  * 3. In what scenario would the counter1 code be preferable? In what scenario would counter2 be better? 
- *
+ * 
+ *      -If you are looking to accomplish a certain task without using a global variable, otherwise counter2 would
+ *  be better.
 */
 
 // counter1 code
@@ -54,17 +62,19 @@ function counter2() {
 
 /* Task 2: inning() 
 
-Write a function called `inning` that generates a random number of points that a team scored in an inning. This should be a whole number between 0 and 2. */
+Write a function called `inning` that generates a random number of points that a team scored in an inning.
+ This should be a whole number between 0 and 2. */
 
-function inning(/*Code Here*/){
-
-    /*Code Here*/
-
+function inning(score){
+  return Math.floor(Math.random() *  Math.floor(score));
 }
+
+console.log(inning(3));
 
 /* Task 3: finalScore()
 
-Write a higher order function called `finalScore` that accepts the callback function `inning` (from above) and a number of innings and and returns the final score of the game in the form of an object.
+Write a higher order function called `finalScore` that accepts the callback function `inning` 
+(from above) and a number of innings and and returns the final score of the game in the form of an object.
 
 For example, 
 
@@ -76,11 +86,25 @@ finalScore(inning, 9) might return:
 
 */ 
 
-function finalScore(/*code Here*/){
+const innings = 9;
 
-  /*Code Here*/
+function finalScore(innings){
+
+  var scoreCard = {Home: 0, Away:0};
+
+  for (let i = 0; i < innings; i++){
+
+    scoreCard[0].Home = scoreCard[0].Home + inning(3);
+
+    scoreCard[0].Away = scoreCard[0].Away + inning(3);
+
+  }
+  
+  return scoreCard;
 
 }
+
+// console.log(finalScore(innings));
 
 /* Task 4: 
 
@@ -103,8 +127,32 @@ and returns the score at each pont in the game, like so:
 
 Final Score: 6 - 10 */
 
-function scoreboard(/* CODE HERE */) {
-  /* CODE HERE */
+function scoreboard(innings) {
+
+  let homeScore = 0;
+  let awayScore = 0;
+
+  for (let i = 1; i <= innings; i++){
+    
+    homeScore = homeScore + inning(3);
+    awayScore = awayScore + inning(3);
+
+    if ( i === 1){
+      console.log(i + "st inning: " + homeScore + " - " + awayScore);
+    }else if( i === 2){
+      console.log(i + "nd inning: " + homeScore + " - " + awayScore);
+
+    }else if( i === 3){
+      console.log(i + "rd inning: " + homeScore + " - " + awayScore);
+    }else{
+      console.log(i + "th inning: " + homeScore + " - " + awayScore);
+
+    }
+
+  }
+
+
 }
 
 
+scoreboard(innings);
